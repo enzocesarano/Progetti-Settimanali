ed747a706e46c3cd736884a9ae3da2f5531257db
EnzoWay <vincenzo.cesarano92@gmail.com>
2024-07-12 17:06:32 +0200

    up

--- UNIT2/Settimana1/Progetto/assets/js/script.js
+++ UNIT2/Settimana1/Progetto/assets/js/script.js
@@ -1,13 +1,20 @@
 const nav = document.getElementsByTagName('nav')[0];
+const list = document.querySelector('nav ul li:nth-of-type(5)');
+
+console.log(list)
 
 window.addEventListener('scroll', function () {
     const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
     if (scrollTop > 500) {
         nav.style.backgroundColor = 'white';
         nav.style.border = 'none';
+        list.classList.remove('black')
+        list.classList.add('green');
     } else {
         nav.style.background = '#FFC017';
         nav.style.borderBottom = '1px solid #191919';
+        list.classList.remove('green');
+        list.classList.add('black');
     }
 });
 
